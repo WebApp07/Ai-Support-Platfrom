cc9518414b57b7323bf5ea9c776286f0c0b40adb
webapp07 <aminezouguig@proton.me>
2025-08-14 13:17:59 +0200

    Enbale Organization in the backend

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -12,9 +12,17 @@ export const add = mutation({
   args: {},
   handler: async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
+    console.log(identity);
     if (identity === null) {
       throw new Error("Not authenticated");
     }
+
+    const orgId = identity.orgId as string;
+
+    if (!orgId) {
+      throw new Error("Missing organization");
+    }
+
     const userId = await ctx.db.insert("users", {
       name: "Amine",
     });
